668e7896fca880664d9b991874b15ef10d0cec7f
Lab1-1 <Lab1-1@DESKTOP-NHSN8JG>
2023-12-21 10:41:17 +0300

    Proje dosyası ekle.

---
+++ BGT_GORSEL_21Ara-2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BGT_GORSEL_21Ara_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(36, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 156);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gün Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 235);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BGT_GORSEL_21Ara-2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGT_GORSEL_21Ara_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Haftanın kaçıncı günü?
+            // DateTime.Now.DayOfWeek
+            int gun;
+            gun = Convert.ToInt32(DateTime.Now.DayOfWeek);
+            
+            switch (gun)
+            {
+                case 0:
+                    MessageBox.Show("Pazar");
+                    break;
+                case 1:
+                    MessageBox.Show("Pazartesi");
+                    break;
+                case 2:
+                    MessageBox.Show("Salı");
+                    break;
+                case 3:
+                    MessageBox.Show("Çarşamba");
+                    break;
+                case 4:
+                    MessageBox.Show("Perşembe");
+                    break;
+                case 5:
+                    MessageBox.Show("Cuma");
+                    break;
+                case 6:
+                    MessageBox.Show("Cumartesi");
+                    break;
+                default:
+                    MessageBox.Show("HATA");
+                    break;
+            }
+        }
+    }
+}
